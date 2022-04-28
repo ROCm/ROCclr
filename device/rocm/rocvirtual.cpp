ROCm/ROCclr

commit 5d6ba0f1440aac5c9582190ed6002009b26dd471

SWDEV-204804 - Detecing pcie atomic support
- check pcie atomci support for printf functionality
- if not enabled printf wont work

Signed-off-by: sdashmiz <shadi.dashmiz@amd.com>
Change-Id: Ib366e8e71772b02210c4a830bca4bd8cc7a11664

device/rocm/rocvirtual.cpp
@@ -2781,7 +2781,7 @@ bool VirtualGPU::submitKernelInternal(const amd::NDRangeContainer& sizes, const 
           break;
         }
         case amd::KernelParameterDescriptor::HiddenHostcallBuffer: {
-          if (amd::IS_HIP) {
+          if (roc_device_.info().pcie_atomics_) {
             uintptr_t buffer = reinterpret_cast<uintptr_t>(
               roc_device_.getOrCreateHostcallBuffer(gpu_queue_, coopGroups, cuMask_));
             if (!buffer) {
@@ -2790,6 +2790,10 @@ bool VirtualGPU::submitKernelInternal(const amd::NDRangeContainer& sizes, const 
               return false;
             }
             WriteAqlArgAt(hidden_arguments, buffer, it.size_, it.offset_);
+          } else {
+            ClPrint(amd::LOG_ERROR, amd::LOG_KERN,
+                    "Pcie atomics not enabled, printf not supported");
+            return false;
           }
           break;
         }

device/device.hpp
@@ -603,6 +603,8 @@ struct Info : public amd::EmbeddedObject {
 
   //! AQL Barrier Value Packet support
   bool aqlBarrierValue_;
+
+  bool pcie_atomics_; //!< Pcie atomics support flag
 };
 
 //! Device settings

device/rocm/rocdevice.cpp
@@ -207,6 +207,16 @@ void Device::setupCpuAgent() {
           system_segment_.handle, system_coarse_segment_.handle, _bkendDevice.handle);
 }
 
+void Device::checkAtomicSupport() {
+  std::vector<amd::Device::LinkAttrType> link_attrs;
+  link_attrs.push_back(std::make_pair(LinkAttribute::kLinkAtomicSupport, 0));
+  if (findLinkInfo(system_segment_, &link_attrs)) {
+    if (link_attrs[0].second == 1) {
+      info_.pcie_atomics_ = true;
+    }
+  }
+}
+
 Device::~Device() {
 #ifdef WITH_AMDGPU_PRO
   delete pro_device_;
@@ -1166,6 +1176,8 @@ bool Device::populateOCLDeviceConstants() {
   assert(info_.globalMemChannels_ > 0);
 
   setupCpuAgent();
+
+  checkAtomicSupport();
 
   assert(system_segment_.handle != 0);
   if (HSA_STATUS_SUCCESS != hsa_amd_agent_iterate_memory_pools(

device/rocm/rocdevice.hpp
@@ -374,6 +374,9 @@ class Device : public NullDevice {
   static const std::vector<AgentInfo>& getCpuAgents() { return cpu_agents_; }
 
   void setupCpuAgent(); // Setup the CPU agent which has the least NUMA distance to this GPU
+
+  void checkAtomicSupport(); //!< Check the support for pcie atomics
+
   //! Destructor for the physical HSA device
   virtual ~Device();
 

commit 7e792ab16f7b27c844ec8f4254958dbe6adb0c3f

SWDEV-307184 - Fix a regression from 5d6ba0f1
Disable hostcall buffer in OCL for now. COv5 can add hostcallbuffer
metadata for unknown reason. OCL may fail the buffer allocation
and kernel launch.

Change-Id: I34a6a45bac86c57422b764c0d69760c96920d6c5

device/rocm/rocvirtual.cpp
@@ -2780,19 +2780,21 @@ bool VirtualGPU::submitKernelInternal(const amd::NDRangeContainer& sizes, const 
           break;
         }
         case amd::KernelParameterDescriptor::HiddenHostcallBuffer: {
-          if (dev().info().pcie_atomics_) {
-            uintptr_t buffer = reinterpret_cast<uintptr_t>(
-              roc_device_.getOrCreateHostcallBuffer(gpu_queue_, coopGroups, cuMask_));
-            if (!buffer) {
+          if (amd::IS_HIP) {
+            if (dev().info().pcie_atomics_) {
+              uintptr_t buffer = reinterpret_cast<uintptr_t>(
+                roc_device_.getOrCreateHostcallBuffer(gpu_queue_, coopGroups, cuMask_));
+              if (!buffer) {
+                ClPrint(amd::LOG_ERROR, amd::LOG_KERN,
+                        "Kernel expects a hostcall buffer, but none found");
+                return false;
+              }
+              WriteAqlArgAt(hidden_arguments, buffer, it.size_, it.offset_);
+            } else {
               ClPrint(amd::LOG_ERROR, amd::LOG_KERN,
-                      "Kernel expects a hostcall buffer, but none found");
+                      "Pcie atomics not enabled, hostcall not supported");
               return false;
             }
-            WriteAqlArgAt(hidden_arguments, buffer, it.size_, it.offset_);
-          } else {
-            ClPrint(amd::LOG_ERROR, amd::LOG_KERN,
-                    "Pcie atomics not enabled, printf not supported");
-            return false;
           }
           break;
         }